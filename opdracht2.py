ff40bd783c33a71af3364beb59710505578d8a6b
Ronald Crooy <r.a.crooy@gmail.com>
2012-06-19 20:04:06 +0200

    opdracht 2 en 4

--- opdracht2.py
+++ opdracht2.py
@@ -1,11 +1,42 @@
-import logging
-import sys
+import unittest
+import re
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO,format=sys.argv[0] + " : %(levelname)s: %(message)s")
+class Square(object):
+    def __init__(self, size = 5):
+        self.size = size
+    def width(self):
+        return self.size
+    def height(self):
+        return self.size
+    def toString(self):
+        x=''
+        for i in range(self.size):
+            for j in range(self.size):
+                x+='*'
+            x+='\n'
+        return x
 
-r=range(100)
-t='I will not talk in class'
 
-for i in r:
-    logger.info(t)
+class SquareTest(unittest.TestCase):
+    def setUp(self):
+        self.givenSize = 5
+        self.square = Square( self.givenSize )
+
+    def testWidth(self):
+        expected = self.givenSize
+        self.assertEqual( expected, self.square.width() )
+    
+    def testHeight(self):
+        expected = self.givenSize
+        self.assertEqual( expected, self.square.width() )
+
+    def testPrintedChars(self):
+        expectedChar = '*'
+        actual = self.square.toString()
+        starsRE = r'(\*{' + str(self.givenSize) + r'}\n){' + str(self.givenSize) + '}'
+        regex = re.compile(starsRE, re.MULTILINE)
+        self.assertTrue(regex.match(actual) != None)
+        
+
+if __name__ == '__main__':
+    unittest.main()
