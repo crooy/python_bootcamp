5661e4129fff87436c0ea0c5e12622294b9d6f52
Ronald Crooy <r.a.crooy@gmail.com>
2012-06-19 18:53:21 +0200

    rename

--- opdracht1.py
+++ opdracht1.py
@@ -1,1 +1,11 @@
-print "Hello World"
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,format=sys.argv[0] + " : %(levelname)s: %(message)s")
+
+r=range(100)
+t='I will not talk in class'
+
+for i in r:
+    logger.info(t)
